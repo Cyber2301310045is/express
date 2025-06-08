137819c81b42404d7d114a23ca7bda51d3fb9a9b
2301310045 <2301310045is@cyber-u.ac.jp>
2025-06-08 23:10:57 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -5,8 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "************";
-const client = new MongoClient(uri);
+const uri = "****************************";
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
